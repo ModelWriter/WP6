e64ae244c395906e122bb683c6e59dd79ca2c9b5
A. Furkan Tanriverdi <ffurkan.tanriverdi@gmail.com>
2015-02-17 13:43:55 +0200

    + implementation cont. + xmi file created with wrong levels

--- Sources/eu.modelwriter.architecture.textconnectors.docx/src/eu/modelwriter/architecture/textconnectors/docx/Docx2EcoreConverter.java
+++ Sources/eu.modelwriter.architecture.textconnectors.docx/src/eu/modelwriter/architecture/textconnectors/docx/Docx2EcoreConverter.java
@@ -1,6 +1,7 @@
 package eu.modelwriter.architecture.textconnectors.docx;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,8 +11,13 @@ import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFStyles;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceFactoryImpl;
 
+import SimpleRequirementMM.Product;
 import SimpleRequirementMM.Requirement;
 import SimpleRequirementMM.RequirementLevel;
 import SimpleRequirementMM.SimpleRequirementMMFactory;
@@ -19,14 +25,25 @@ import SimpleRequirementMM.SimpleRequirementMMFactory;
 public class Docx2EcoreConverter {
 
 	private static Resource resource;
+	private static int reqId = 0;
+	private final static String REQUIREMENT_NAME = "Name ";
+	private final static String REQUIREMENT_DESCRIPTION = "Description ";
+	private final static String REQUIREMENT_REFINE = "Refine ";
+	private final static String REQUIREMENT_DEPENDENCY_TO = "Dependency to ";
+	private final static String REQUIREMENT_PRIORITY = "Priority ";
+
+	private static Map<Requirement,String> requirementsMap;
+	private static Stack<RequirementLevel> requirementLevelStack;
+	private static Map<RequirementLevel,Integer> requirementLevelMap;
+	private static Map<String,Integer> headingMap;
 
 	public static void main(String[] args)throws Exception 
 	{
+		requirementsMap = new HashMap<Requirement,String>();
+		requirementLevelStack = new Stack<RequirementLevel>();
 
-		Stack<RequirementLevel> requirementLevelStack = new Stack<RequirementLevel>();
-
-		Map<RequirementLevel,Integer> requirementLevelMap = new HashMap<RequirementLevel, Integer>();
-		Map<String,Integer> headingMap = new HashMap<String,Integer>();
+		requirementLevelMap = new HashMap<RequirementLevel, Integer>();
+		headingMap = new HashMap<String,Integer>();
 
 		headingMap.put("NoSpacing", 0);
 		headingMap.put("Heading1", 1);
@@ -68,46 +85,147 @@ public class Docx2EcoreConverter {
 		 */
 
 		int controller = 0;
-
+		Product product = factory.createProduct();
+		boolean reqFlag = false;
+		Requirement r = null;
+		
 		for(XWPFParagraph para : paragraphList){
 
+			controller++;
 			String t = para.getText();
-			
+
 			if( para != null){
 
-				if(controller == 0){
+				if(controller == 1){
 
 					RequirementLevel rl = factory.createRequirementLevel();
-					rl.setName(para.getStyle());
+					rl.setName(para.getText());
+
+					product.getOwnedRequirementLevel().add(rl);
+
 					requirementLevelStack.push(rl);
-					requirementLevelMap.put(rl, headingMap.get(para.getStyle()));
-					controller++;
+					requirementLevelMap.put(rl, headingMap.get(para.getStyle()));					
+				}				
+
+				else {
+
+					// bu en alt seviyede yer alan req informations ise
+					if(para.getStyle() == null){
+						
+						if(reqFlag == false){
+							
+							r = factory.createRequirement();
+							r.setName(requirementLevelStack.peek().getName());
+							r.setId(reqId++);
+							
+							reqFlag = true;
+						}															
+
+						String[] values = para.getText().split(":");
+
+						if(values[0].equals(REQUIREMENT_NAME)){
+
+							r.setName(values[1]);
+						}
+
+						if(values[0].equals(REQUIREMENT_DESCRIPTION)){
+
+							r.setDescription(values[1]);
+						}
+
+						if(values[0].equals(REQUIREMENT_DEPENDENCY_TO)){
+
+							// daha tanýmlanmamýþ olan bir req olmasý durumu ?
+							// r.setDependencyTo();
+						}
+
+						if(values[0].equals(REQUIREMENT_PRIORITY)){
+
+							// priority nasýl baðlýcaz ?
+						}
+
+
+					}else{
+						
+						if(reqFlag == true){
+							
+							RequirementLevel poppedReqLvl = requirementLevelStack.pop();
+							poppedReqLvl.getOwnedRequirement().add(r);
+							requirementLevelStack.peek().getOwnedLevel().add(poppedReqLvl);
+							
+							reqFlag = false;
+
+						}
+						
+						
+						// yeni okuduðum satýrýn levelý 
+						// stackteki en üstte bulunan leveldan seviye olarak daha düþükse
+						// map deðeri büyükse
+
+						if(headingMap.get(para.getStyle()) > requirementLevelMap.get(requirementLevelStack.peek())){
+							
+							RequirementLevel newReqLvl = factory.createRequirementLevel();
+							newReqLvl.setName(para.getText());
+
+							product.getOwnedRequirementLevel().add(newReqLvl);
+
+							requirementLevelStack.push(newReqLvl);
+							requirementLevelMap.put(newReqLvl, headingMap.get(para.getStyle()));
+						}
+
+						// eþit olma durumu
+
+						else if(controller > 1 && (headingMap.get(para.getStyle()) == requirementLevelMap.get(requirementLevelStack.peek()))){
+							
+							RequirementLevel poppedReqLvl = requirementLevelStack.pop();
+
+							if(!requirementLevelStack.isEmpty()){
+
+								requirementLevelStack.peek().getOwnedLevel().add(poppedReqLvl);
+							}else{
+
+								product.getOwnedRequirementLevel().add(poppedReqLvl);
+							}
+
+							RequirementLevel newReqLvl = factory.createRequirementLevel();
+							newReqLvl.setName(para.getText());
+
+							product.getOwnedRequirementLevel().add(newReqLvl);
+
+							requirementLevelStack.push(newReqLvl);
+							requirementLevelMap.put(newReqLvl, headingMap.get(para.getStyle()));
+						}
+
+						// geri dönüþ
+						// elimizdeki level daha büyükse stackteki req. levellarý(eþit olan da dahil)
+						// geri dönük bir þekilde bir üstlerindeki req level listelerine ekliyoruz.
+
+						else{											
+
+							while(headingMap.get(para.getStyle()) <= requirementLevelMap.get(requirementLevelStack.peek())){
+								
+								RequirementLevel poppedReqLvl = requirementLevelStack.pop();
+								requirementLevelStack.peek().getOwnedLevel().add(poppedReqLvl);
+
+							}
+
+							// elimizdeki yeni req lvl ý oluþturuyoruz.
+							RequirementLevel newReqLvl = factory.createRequirementLevel();
+							newReqLvl.setName(para.getText());
+
+							product.getOwnedRequirementLevel().add(newReqLvl);
+
+							requirementLevelStack.push(newReqLvl);
+							requirementLevelMap.put(newReqLvl, headingMap.get(para.getStyle()));
+
+						}
+					}
+
+					
+
 				}
 
-				
-				
-				// yeni okuduðum satýrýn levelý 
-				// stackteki en üstte bulunan leveldan seviye olarak daha düþükse
-				// map deðeri büyükse
 
-				if(para.getStyle() == null){
-
-					Requirement r = factory.createRequirement();
-					String[] values = para.getText().split(":");
-					
-					
-
-				} else if(headingMap.get(para.getStyle()) > requirementLevelMap.get(requirementLevelStack.peek())){
-
-					RequirementLevel newRl = factory.createRequirementLevel();
-					newRl.setName(para.getText());
-					requirementLevelStack.push(newRl);
-					requirementLevelMap.put(newRl, headingMap.get(para.getStyle()));
-
-				}
-				
-				// eþit olma durumu
-				// geri dönüþ
 
 
 
@@ -115,7 +233,39 @@ public class Docx2EcoreConverter {
 
 		}
 
+		createXMIFile(product);
 
+
+	}
+
+	private static void createXMIFile(Product product) {
+		// TODO Auto-generated method stub
+
+				ResourceSet resourceSet = new ResourceSetImpl();
+				/*
+				 * Register XML Factory implementation using DEFAULT_EXTENSION
+				 */
+				resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
+						"xmi", new  XMLResourceFactoryImpl());
+
+				/*
+				 * Create empty resource with the given URI
+				 */
+				Resource resource = resourceSet.createResource(URI.createURI("Model/SimpleRequirementMM.xmi"));
+
+				/*
+				 * Add bookStoreObject to contents list of the resource 
+				 */
+				resource.getContents().add(product);
+
+				try{
+					/*
+					 * Save the resource
+					 */
+					resource.save(null);
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
 	}
 
 
