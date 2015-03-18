28a02300c5f03e19e3d66bca8ab30cd6ae767282
Ferhat Erata <ferhat.erata@unitbilisim.com>
2015-03-18 13:41:16 +0200

    markdown and textile

--- Sources/eu.modelwriter.architecture.textconnectors.docx.parser/src/eu/modelwriter/architecture/textconnectors/docx/parser/Test.java
+++ Sources/eu.modelwriter.architecture.textconnectors.docx.parser/src/eu/modelwriter/architecture/textconnectors/docx/parser/Test.java
@@ -7,12 +7,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.mylyn.docs.intent.markup.builder.ModelDocumentBuilder;
 import org.eclipse.mylyn.wikitext.core.parser.DocumentBuilder;
 import org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
@@ -23,25 +25,35 @@ public class Test {
 
 	public static void main(String[] args) throws IOException {
 
-		byte[] encoded = Files.readAllBytes(Paths.get("testdata/my.textile"));
+		//byte[] encoded = Files.readAllBytes(Paths.get("testdata/my.textile"));
+		byte[] encoded = Files.readAllBytes(Paths.get("testdata/my.markdown"));
 		String markupContent = new String(encoded, Charset.defaultCharset());
 		
 		ModelDocumentBuilder builder = new ModelDocumentBuilder();
 
 		MarkupParser markupParser = new MarkupParser();
-		markupParser.setMarkupLanguage(new TextileLanguage());
+		//markupParser.setMarkupLanguage(new TextileLanguage());
+		markupParser.setMarkupLanguage(new MarkdownLanguage());
 		markupParser.setBuilder(builder);
 		
 		markupParser.parse(markupContent);
+	
 		
 		Collection<EObject> correspondingModels = builder.getRoots();
 		
+		
+		// Register the XMI resource factory for the .website extension
+
+	    Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+	    Map<String, Object> m = reg.getExtensionToFactoryMap();
+	    m.put("markup", new XMIResourceFactoryImpl());
 		// Obtain a new resource set
 	    ResourceSet resSet = new ResourceSetImpl();
 	    // create a resource
-	    Resource resource = resSet.createResource(URI.createURI("testdata/my.xmi"));
+	    //Resource resource = resSet.createResource(URI.createURI("model/my_textile.markup"));
+	    Resource resource = resSet.createResource(URI.createURI("model/my_markdown.markup"));
 	    resource.getContents().addAll(correspondingModels);
-	    	    // now save the content.
+	    // now save the content.
 	    try {
 	      resource.save(Collections.EMPTY_MAP);
 	    } catch (IOException e) {
